f347c160a84333cb3a08fdb88185e6c5fd7757e4
Danrley <danrleysantos02@gmail.com>
2022-11-10 22:52:36 -0300

    Atualização

---
+++ Controllers/Cadastro.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sistema_de_cadastro.Controllers
+{
+    public class Cadastro : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
